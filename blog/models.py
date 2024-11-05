aa00252f2142598836b3da1f1c22fa910b091160
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-05 11:24:40 +0530

    Added Meta class

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,21 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     """create a blog model"""
 
+    class Meta:
+        ordering = ("id",)
+
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
 
 
 class Blog(models.Model):
     """create a blog model"""
+
+    class Meta:
+        ordering = ("created_at",)
 
     author = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
     category = models.ForeignKey(
@@ -23,3 +32,6 @@ class Blog(models.Model):
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
