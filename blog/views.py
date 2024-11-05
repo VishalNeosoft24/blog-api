382f0d952d5ac4966c9e437b4008826c9a9e6e53
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-05 11:26:59 +0530

    Create Category and Blog View

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,43 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import BlogSerializer, CategorySerializer
+from .models import Blog, Category
 
 
 # Create your views here.
-def test(request):
-    return HttpResponse("Hello world")
+class CategoryView(APIView):
+    """CategoryCreateview"""
+
+    def post(self, request):
+        """post request"""
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """get request"""
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class BlogView(APIView):
+    """BlogView"""
+
+    def get(self, request):
+        """get request"""
+        categories = Blog.objects.all()
+        serializer = BlogSerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """post request"""
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
