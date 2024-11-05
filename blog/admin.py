d7cc4f2a31940ca64828e0d3fbb3f97c0e283498
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-05 11:24:21 +0530

    register Category and Blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Blog, Category
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """CategoryAdmin"""
+
+    list_display = [
+        "id",
+        "name",
+        "description",
+    ]
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    """BlogAdmin"""
+
+    list_display = ["id", "author", "category", "title", "content"]
